b972a756099dd0d90e7c4db87f8f895275b5a80d
Seungwoo Jang <se93wo@gmail.com>
2024-08-06 11:08:47 +0900

    Add files via upload

---
+++ install_1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+eval "$(conda shell.bash hook)"
+
+conda init
+source ../.bashrc
+
+sudo apt-get update
+sudo ln -snf /usr/share/zoneinfo/$CONTAINER_TIMEZONE /etc/localtime
+sudo apt-get install -y libsm6 libxext6 ffmpeg zip 
+
+conda create -n tabular python==3.10 -y
+conda activate tabular
+conda install anaconda::ipywidgets -y 
+conda install ipykernel -y
+python -m ipykernel install --user --name tabular 
+pip install autogluon.eda
+conda deactivate
+
+conda create -n cv python==3.10 -y
+conda activate cv
+pip install -U pip
+pip install -U setuptools wheel
+conda install anaconda::ipywidgets -y 
+conda install ipykernel -y
+python -m ipykernel install --user --name cv 
+pip install autogluon==1.1.0
+mim install mmcv==2.1.0
+pip install mmdet
+conda install numpy -y 
+conda deactivate
